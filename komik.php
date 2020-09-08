eb141095e165f4538261a65e0ef99f7064d4c9af
MR.ARIS <70155682+ariszywlafa1@users.noreply.github.com>
2020-09-08 23:37:50 +0700

    Create komik.php

---
+++ komik.php
@@ -0,0 +1,87 @@
+<?php
+         // ngapain tong,jangan recode
+         // NGAPAIN NANO AMJINF
+         // Print out main menu..
+    system('clear');
+    $green  = "\e[92m";
+    $red    = "\e[91m";
+    $yellow = "\e[93m";
+    $blue   = "\e[36m";
+    echo $red. "
+                       =========================
+                       |       KOMIK DEWASA    |
+                       |     AUTHOR :MR.ARIS   |
+                       |  WHATSAPP:08884196160 |
+                       =========================
+                                 |    |
+                                 |    |\n";
+    echo $yellow. "
+                    ================================
+                    |  SCRIPT RUSAK HUBUNGI ADMIN  |
+                    ================================
+                    |     RECODE MANDUL HAYYUUKK   |
+                    ================================\n";
+    echo $green. "
+     ====================================================
+     |      [   MADLOKI   ]    |     [  CAMPURAN  ]     |
+     |   1) CERITA CITRA EPS 1 |  8)  TANTEKU MONTOK    |
+     |   2) CERITA CITRA EPS 2 |  9)  NARUTO X SAKURA   |
+     |   3) CERITA CITRA EPS 3 |  10) CERITA MOM 2      |
+     |   4) UPACARA SEKOLAH    |  11) CERITA MOM 3      |
+     |   5) INDOMARET          |  12) NIDURIN ANAK      |
+     |   6) ICE CREAM          |  13) SASUKE X SAKURA   |
+     |   7) TEMAN SEKELAS      |  14) TSUNADE X NARUTO  |
+     ====================================================
+     TEKAN ENTER UNTUK KELUAR DARI SCRIPT\n";
+    echo $blue."
+        Pilih Nomor Yang Ingin Didownload :\n";
+    switch(trim(fgets(STDIN,256)))
+        {
+            case 1:
+                echo "LINK NYA NIH GAN [ https://bit.ly/3h8Worc ]";
+                exit();
+            case 2:
+                echo "LINK NYA NIH GAN [ https://bit.ly/3jUYXyB ]";
+                exit();
+            case 3:
+                echo "LINK NYA NIH GAN [ https://bit.ly/3bz5oVi ]";
+                exit();
+            case 4:
+                echo "LINK NYA NIH GAN [ https://bit.ly/3bzn7fk  ]";
+                exit();
+            case 5:
+                echo "LINK NYA NIH GAN [ https://bit.ly/35sUL5x ]";
+                exit();
+            case 6:
+                echo "LINK NYA NIH GAN [ https://bit.ly/3h7y0Gf ]";
+                exit();
+            case 7:
+                echo "LINK NYA NIH GAN [ https://bit.ly/35hoOgk ]";
+                exit();
+            case 8:
+                echo "BELUM TERSEDIA";
+                exit();
+            case 9:
+                echo "BELUM TERSEDIA";
+                exit();
+            case 10:
+                echo "LINK NYA NIH GAN [ https://bit.ly/32aHhch ]";
+                ext();
+            case 11:
+                echo "LINK NYA NIH GAN [ https://bit.ly/336C3Od ]";
+                exit();
+            case 12:
+                echo "BELUM TERSEDIA";
+                exit();
+            case 13:
+                echo "BELUM TERSEDIA";
+                exit();
+            case 14:
+                echo "BELUM TERSEDIA";
+                exit();
+            case "0":
+                exit();
+        }
+    // Close standard in..
+    fclose(STDIN);
+?>
